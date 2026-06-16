fdcb753e5e41e9f0821ec409011bff7972ce04c9
hjhhoni <hjhhoni@gmail.com>
2026-06-16 09:25:10 +0800

    11

--- js基础/异步/6.json对象.js
+++ js基础/异步/6.json对象.js
@@ -1,9 +1,9 @@
-var str_json = '{"name":"张三","age":18}';
-console.log(str_json, typeof str_json);
+var str_json = '{"name":"张三","age":18}';  
+console.log(str_json, typeof str_json);  // 这个时候还是字符串string
 
 // 字符串转对象
 var json_obj = JSON.parse(str_json);
-console.log(json_obj, typeof json_obj);  // 可以完成我们的数据加密定位
+console.log(json_obj, typeof json_obj);  // 这个JSON.parse()可以把字符串转换为对象，很多爬虫
 console.log(json_obj.name);
 
 
