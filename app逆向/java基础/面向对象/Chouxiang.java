957dfed870e91fb139048225dcfc4a7076a64063
hjhhoni <hjhhoni@gmail.com>
2026-06-23 22:16:43 +0800

    1

---
+++ app逆向/java基础/面向对象/Chouxiang.java
@@ -0,0 +1,8 @@
+// import java.util.Random;
+
+public class Chouxiang {
+    public static void main(String[] args){
+        System.out.print(Math.random()*1000);
+        // System.out.print();
+    }
+}
