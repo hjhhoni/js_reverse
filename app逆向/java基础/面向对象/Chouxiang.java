38679c18adc09e0f1149faa9f94a17dfe8f88d89
hjhhoni <hjhhoni@gmail.com>
2026-06-24 10:25:06 +0800

    1

--- app逆向/java基础/面向对象/Chouxiang.java
+++ app逆向/java基础/面向对象/Chouxiang.java
@@ -2,6 +2,10 @@
 
 public class Chouxiang {
     public static void main(String[] args){
+        String a = "11.11";
+        Double d = new Double(a);
+        double r = d.doubleValue();
+        System.out.println(d+1.1);
         System.out.print(Math.random()*1000);
         // System.out.print();
     }
