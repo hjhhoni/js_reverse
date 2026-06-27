e553d730d46c4d0efb77321ed0b4b571df1fa802
hjhhoni <hjhhoni@gmail.com>
2026-06-27 18:54:20 +0800

    1

---
+++ 副业/自动化/知了标讯26.6.27/spider.js
@@ -0,0 +1,11 @@
+// 1782551136572;
+
+
+
+
+var d = t[c]({
+    code: 1,
+    msg: 'OK',
+    data: Math.floor(Date.now() / 1000), // 时间戳
+});
+f = d.value;
---
+++ 副业/自动化/知了标讯26.6.27/对比.py
@@ -0,0 +1,37 @@
+import requests
+
+
+headers = {
+    "Accept": "application/json, text/plain, */*",
+    "Accept-Language": "en,zh;q=0.9,zh-CN;q=0.8",
+    "Cache-Control": "no-cache",
+    "Connection": "keep-alive",
+    "Content-Type": "application/json",
+    "Origin": "https://www.zhiliaobiaoxun.com",
+    "Pragma": "no-cache",
+    "Referer": "https://www.zhiliaobiaoxun.com/search?key=12345",
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Site": "cross-site",
+    "Sec-Fetch-Storage-Access": "active",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/149.0.0.0 Safari/537.36",
+    "auth-token": "eyJhbGciOiJIUzUxMiIsInppcCI6IkdaSVAifQ.H4sIAAAAAAAAAKtWykwsUbIyNLcwMjUxNDYy0VEqLk1SslIyMjA3NDZT0lFKrSgAylsYmhiYmwPlawHHOQwsMgAAAA.D9VKK0v03eWkUWHGi9UAtM4ZDP2xAlmEIEqOL547pFwpNYn9yAhfWuRUapPmBEd1K6Q-dHsKAeLtFWjSeogb4Q",
+    "platform": "pc",
+    "sec-ch-ua": "\"Google Chrome\";v=\"149\", \"Chromium\";v=\"149\", \"Not)A;Brand\";v=\"24\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Windows\""
+}
+url = "https://api-service-zhiliao.bailian-ai.com/search/bid"
+params = {
+    "userId": "207136",
+    "page": "3",
+    "count": "10",
+    "bidProcesses": "70",
+    "keyword": "12345",
+    "timestamp": "1782544362545",
+    "hash": "1b33af8ef6ac28f71646da6bf5c6e660"
+}
+response = requests.get(url, headers=headers, params=params)
+
+print(response.text)
+print(response)
